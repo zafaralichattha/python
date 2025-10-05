fa8166889d123c3a3e3acbf3ebe446a6a7b7c275
zafaralichattha <mastermathstwelve@gmail.com>
2025-10-06 01:32:10 +0500

    This is basic input and array format

--- python1.py
+++ python1.py
@@ -1,3 +1,18 @@
 
 name =input("Please enter your name:")
 print(name)
+print("And the type of name is:",type(name))
+
+age =int(input("Please enter your age:"))
+print("And the type of age is:",type(age))
+print(age)
+sex=None
+
+
+details=[name,age,sex]
+print("The name and age in array is:",details)
+
+length=len(name)
+print("The length of name is:",length)
+length1=len(details)
+print("The length of array veriables is:",length1)
